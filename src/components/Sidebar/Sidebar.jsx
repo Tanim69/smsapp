19b8c3822252291c3a84ecf99f9d3f034eb93e36
tanim <tanimkhan117@gmail.com>
2023-11-07 22:13:33 +0600

    image cropping done etc

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import profile from '../../assets/profile.png'
 import { AiOutlineHome, AiOutlineMessage, AiOutlineSetting } from 'react-icons/ai'
 import { IoIosNotificationsOutline } from 'react-icons/io'
@@ -6,6 +6,15 @@ import { IoLogOut } from 'react-icons/io5'
 
 import { getAuth, signOut } from "firebase/auth";
 import { useNavigate } from 'react-router-dom'
+import { BsFillCloudUploadFill } from 'react-icons/bs'
+import { useState } from 'react'
+
+
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+
+
+
 
 
 const Sidebar = () => {
@@ -25,9 +34,66 @@ const Sidebar = () => {
             console.log(error.code);
         });
     }
+    const [imageUploadPopup, setImageUploadPopup] = useState(false)
+
+    const [image, setImage] = useState();
+    const [cropData, setCropData] = useState();
+    const cropperRef = createRef();
+
+
+    const handleImageUpload = () => {
+        console.log('immggg');
+        setImageUploadPopup(true)
+    }
+
+    const handlecanceluploadpopup = () => {
+        console.log('immggg');
+        setImageUploadPopup(false)
+    }
+
+
+
+    const handleImageChange = (e) => {
+        e.preventDefault();
+        let files;
+        if (e.dataTransfer) {
+            files = e.dataTransfer.files;
+        } else if (e.target) {
+            files = e.target.files;
+        }
+        const reader = new FileReader();
+        reader.onload = () => {
+            setImage(reader.result);
+        };
+        reader.readAsDataURL(files[0]);
+    };
+
+
+
+
+
+
+
+
     return (
         <div className='bg-primary h-screen rounded-lg pt-[38px] '>
-            <img src={profile} alt="" className='mx-auto' />
+
+            <div className='group relative w-[100px] h-[100px] mx-auto '>
+
+                <img src={profile} alt="" className='mx-auto' />
+                <div onClick={handleImageUpload} className='opacity-0 group-hover:opacity-100 cursor-pointer bg-overlay rounded-full w-full h-full absolute top-0 left-0 flex justify-center items-center  transition ease-in-out'>
+
+                    <BsFillCloudUploadFill className='text-[30px] text-[white]' />
+                </div>
+            </div>
+
+
+
+
+
+
+
+
 
             <div className='relative bg-white mt-[78px] ml-[25px] py-[20px] rounded-l-lg '>
 
@@ -50,6 +116,63 @@ const Sidebar = () => {
             <div className='mt-[78px]'>
                 <IoLogOut onClick={handleSignOut} className='mx-auto text-5xl text-[#BAD1FF]' />
             </div>
+
+
+            {
+                imageUploadPopup &&
+
+                <div className='h-screen bg-primary w-full absolute top-0 left-0 z-[999] flex justify-center items-center'>
+                    <div className=' bg-white w-[700px] p-[20px]'>
+
+                        <h1 className='font-nunito font-bold text-[50px]  text-primary'>Image Upload </h1>
+
+
+
+                        <div className=' w-[100px] h-[100px] rounded-full mx-auto overflow-hidden mb-[20px]'>
+
+                            <div className="img-preview w-[100px] h-[100px] rounded-full"></div>
+                        </div>
+
+                        {
+                            image &&
+                            <Cropper
+                                ref={cropperRef}
+                                style={{ height: 400, width: "100%" }}
+                                zoomTo={0.5}
+                                initialAspectRatio={1}
+                                preview=".img-preview"
+                                src={image}
+                                viewMode={1}
+                                minCropBoxHeight={10}
+                                minCropBoxWidth={10}
+                                background={false}
+                                responsive={true}
+                                autoCropArea={1}
+                                checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+                                guides={true}
+                            />
+
+
+                        }
+
+
+
+
+                        <input type="file" className='mt-[30px]' onChange={handleImageChange} />
+
+                        <div className='mt-[10px]'>
+                            <button className='mt-[20px] rounded bg-primary px-[30px] py-[10px] text-white text-[18px]'>Upload</button>
+
+                            <button onClick={handlecanceluploadpopup} className='mt-[20px] rounded bg-red-500 px-[30px] py-[10px] text-white text-[18px] ml-[20px]'>Cancel</button>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            }
+
+
 
 
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ export default {
       colors:{
         'primary':'#5F35F5',
         'shadow':' 0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+        'overlay':'rgba(0, 0, 0, 0.41)',
       }
       
     },
